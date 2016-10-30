31f7e96d51b225af694aad7375c285488eb80619
Olivier Farlotti <olivierfarlotti@gmail.com>
2016-10-30 15:24:41 +0100

    fix: update client build

--- server/server.js
+++ server/server.js
@@ -7,10 +7,12 @@ console.log('Server start');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), 'localhost', function() {
   console.log('App is running, server is listening on port ', app.get('port'));
 });
