23be96dda0c4b75c2bffa9f4c61e6c8fbd2aadb6
Olivier Farlotti <olivierfarlotti@gmail.com>
2016-10-30 10:33:24 +0100

    fix Add server port config

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-console.log('Server up')
+
+console.log('Server start');
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
